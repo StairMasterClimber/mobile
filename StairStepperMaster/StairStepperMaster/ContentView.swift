8390a317c08816adab5b40ec3c02d5438884dca5
philparkus <hiteshparikhus@gmail.com>
2022-05-24 14:35:40 -0400

    Blank app template

---
+++ StairStepperMaster/StairStepperMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StairStepperMaster
+//
+//  Created by Hitesh Parikh on 5/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StairStepperMaster/StairStepperMaster/StairStepperMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StairStepperMasterApp.swift
+//  StairStepperMaster
+//
+//  Created by Hitesh Parikh on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct StairStepperMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
