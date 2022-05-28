8a3a662bde734ae97424c8501b053bf30563dc3d
Saamer Mansoor <i@saamer.me>
2022-05-28 00:22:51 -0400

    StepperMachineTile (#2)
    * Added the stair stepper machine tile to the dashboard page
    * Improved the ButtonStyles to make it more reusable
    * Got data from health kit including last 30 days of stairs and the last VO2 reading
    * Fixed the logic so that we actually get the selection view to function properly with tappable buttons

--- StairStepperMaster/StairStepperMaster/DashboardView.swift
+++ StairStepperMaster/StairStepperMaster/DashboardView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct DashboardView: View {
+    @AppStorage("VO2Max") var vo2Max:Double = 0
+    @AppStorage("FlightsClimbed") var flightsClimbed:Double = 0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack(alignment: .leading){
+                HStack{
+                    Circle()
+                        .frame(width: 96, height: 96)
+                        .foregroundColor(.red)
+                    VStack(alignment: .leading){
+                        Text("Hello James,")
+                            .foregroundColor(.white)
+                            .font(Font.custom("Avenir", size: 24))
+                            .fontWeight(.heavy)
+                        Text("It’s sunny today in SF, \nyou should take a walk :) ")
+                            .foregroundColor(.white)
+                            .font(Font.custom("Avenir", size: 14))
+                        Text("last sync: just now")
+                            .foregroundColor(.white)
+                            .font(Font.custom("Avenir", size: 14))
+                            .fontWeight(.thin)
+                    }
+                }
+                if (vo2Max == 0){
+                    Text("You don't have any VO2 Max data registered. You can get that data by using an Apple Watch").foregroundColor(.white)
+                }
+                else{
+                    Text("Your VO2 Max is " + String(vo2Max)).foregroundColor(.white)
+                }
+                Text("Your flights climbed in the last 30 days is " + String(flightsClimbed)).foregroundColor(.white)
+                MachineTileView()
+            }
+        }
+        .background(ZStack{
+            Image("ScreenBackground").aspectRatio(contentMode: .fit).border(.black)
+        }.background(Color("BrandBlack")))
     }
 }
 
--- StairStepperMaster/StairStepperMaster/SelectionView.swift
+++ StairStepperMaster/StairStepperMaster/SelectionView.swift
@@ -6,11 +6,143 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct SelectionView: View {
-    @State private var isEnabled:Int = 43
-    @AppStorage("DidShowSelectionView") var isActive:Bool = false
+    
+    func fetchHealthData() -> Void
+    {
+        let HKStore = HKHealthStore()
+        
+        if HKHealthStore.isHealthDataAvailable()
+        {
+            let readData = Set([
+                HKObjectType.quantityType(forIdentifier: .heartRate)!,
+                HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+//                HKObjectType.quantityType(forIdentifier: .restingHeartRate)!,
+                HKObjectType.quantityType(forIdentifier: .stepCount)!,
+                HKObjectType.quantityType(forIdentifier: .flightsClimbed)!,
+                HKObjectType.quantityType(forIdentifier: .vo2Max)!,
+//                HKObjectType.quantityType(forIdentifier: .walkingHeartRateAverage)!,
+                HKObjectType.quantityType(forIdentifier: .stairAscentSpeed)!,
+                HKObjectType.quantityType(forIdentifier: .stairDescentSpeed)!,
+                HKObjectType.categoryType(forIdentifier: .highHeartRateEvent)!])
+            let writeData = Set([
+                HKObjectType.quantityType(forIdentifier: .stepCount)!,
+                HKObjectType.quantityType(forIdentifier: .flightsClimbed)!])
+            HKStore.requestAuthorization(toShare: writeData, read: readData) {(success, error) in
+                if success
+                {
+                    let cal = NSCalendar.current
+                    var anchorComps = cal.dateComponents([.day, .month, .year, .weekday], from: NSDate() as Date)
+                    let offset = (5 + anchorComps.weekday! - 2) % 5
+                    let endDate = Date()
+                    
+                    anchorComps.day! -= offset
+                    anchorComps.hour = 1
+                    
+                    guard let anchorDate = Calendar.current.date(from: anchorComps)
+                    else
+                    {
+                        fatalError("Can't get a valid date from the achor. You fucked something up!")
+                    }
+                    
+                    guard let startDate = cal.date(byAdding: .month, value: -1, to: endDate)
+                            else
+                            {
+                                fatalError("Can't generate a startDate! :-/")
+                            }
+                    print(startDate)
+                    print(endDate)
+                    let interval = NSDateComponents()
+                    interval.hour = 24
+                    
+                    guard let quantityType2 = HKObjectType.quantityType(forIdentifier: .flightsClimbed)
+                    else{
+                        fatalError("Can't get quantityType forIdentifier: .flightsClimbed!")
+                    }
+                    guard let quantityType = HKObjectType.quantityType(forIdentifier: .vo2Max)
+                    else
+                    {
+                        fatalError("Can't get quantityType forIdentifier: .vo2Max!")
+                    }
+                    let HKquery = HKStatisticsCollectionQuery(quantityType: quantityType, quantitySamplePredicate: nil, options: .discreteAverage, anchorDate: anchorDate, intervalComponents: interval as DateComponents)
+                    let HKquery2 = HKStatisticsCollectionQuery(quantityType: quantityType2, quantitySamplePredicate: nil, options: .cumulativeSum, anchorDate: anchorDate, intervalComponents: interval as DateComponents)
+                    flightsClimbed = 0
+                    
+                    HKquery2.initialResultsHandler =
+                    {
+                        query, results, error in
+                        guard let statsCollection = results
+                        else
+                        {
+                            fatalError("Unable to get results! Reason: \(String(describing: error?.localizedDescription))")
+                        }
+                        
+                        statsCollection.enumerateStatistics(from: startDate, to: endDate)
+                        {
+                            statistics, stop in
+                            if let quantity = statistics.sumQuantity()
+                            {
+                                print(quantity)
+                                let date = statistics.startDate
+                                let val = quantity.doubleValue(for: HKUnit(from: "count"))
+                                flightsClimbed = val + flightsClimbed
+                                print(val)
+                                print(date)
+                                self.isActive = true
+                            }
+                        }
+                        
+                    }
+                    HKStore.execute(HKquery2)
+                    
+                    HKquery.initialResultsHandler =
+                    {
+                        query, results, error in
+                        guard let statsCollection = results
+                        else
+                        {
+                            fatalError("Unable to get results! Reason: \(String(describing: error?.localizedDescription))")
+                        }
+                        
+                        statsCollection.enumerateStatistics(from: startDate, to: endDate)
+                        {
+                            statistics, stop in
+                            if let quantity = statistics.averageQuantity()
+                            {
+                                print(quantity)
+                                let date = statistics.startDate
+                                let val = quantity.doubleValue(for: HKUnit(from: "mL/min·kg"))
+                                vo2Max = val
+                                self.isActive = true
+                            }
+                        }
+                        
+                    }
+                    HKStore.execute(HKquery)
 
+                }
+                else
+                {
+                    print("Unauthorized!")
+                }
+            }
+        }
+        else
+        {
+            print("ERROR: Unable to fetch data!")
+        }
+    }
+
+    @AppStorage("ActivityGoal") var activityGoal:Int = 8
+//    @State private var activityGoal:Int = 8
+//    @AppStorage("DidShowSelectionView") var isActive:Bool = false
+    @State var isActive:Bool = false
+    var valHR = 0.0
+    var heartCount = 0.0
+    @AppStorage("VO2Max") var vo2Max:Double = 0
+    @AppStorage("FlightsClimbed") var flightsClimbed:Double = 0
     var body: some View {
         if self.isActive {
             // 3.
@@ -26,41 +158,41 @@ struct SelectionView: View {
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
-                    Button(action: {print("asf")}, label: {
+                    Button(action: {activityGoal = 5}, label: {
                         VStack{
                             Text("Lightly")
                                 .fontWeight(.heavy)
                                 .font(Font.custom("Avenir", size: 24))
-                            Text("Little to no exercise")
+                            Text("Little to no exercise (5 flights/day)")
                                 .fontWeight(.thin)
                                 .font(Font.custom("Avenir", size: 14))
                         }
                     })
-                    .buttonStyle(GreyButton())
+                    .buttonStyle(SelectionButton(isActive: activityGoal == 5))
 
-                    Button(action: {print("asf")}, label: {
+                    Button(action: {activityGoal = 8}, label: {
                         VStack{
                             Text("Moderately")
                                 .fontWeight(.heavy)
                                 .font(Font.custom("Avenir", size: 24))
-                            Text("Somewhat physically active")
+                            Text("Somewhat physically active (8 flights/day)")
                                 .fontWeight(.thin)
                                 .font(Font.custom("Avenir", size: 14))
                         }
                     })
-                    .buttonStyle(OrangeButton())
+                    .buttonStyle(SelectionButton(isActive: activityGoal == 8))
 
-                    Button(action: {print("asf")}, label: {
+                    Button(action: {activityGoal = 10}, label: {
                         VStack{
                             Text("Highly")
                                 .fontWeight(.heavy)
                                 .font(Font.custom("Avenir", size: 24))
-                            Text("Dedicated work out routine")
+                            Text("Dedicated work out routine (10 flights/day)")
                                 .fontWeight(.thin)
                                 .font(Font.custom("Avenir", size: 14))
                         }
                     })
-                    .buttonStyle(GreyButton())
+                    .buttonStyle(SelectionButton(isActive: activityGoal == 10))
 
                     Text("A flights of stairs is counted as approximately 10 feet (3 meters) of elevation gain")
                         .font(Font.custom("Avenir", size: 14))
@@ -69,8 +201,8 @@ struct SelectionView: View {
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
                     Button(action: {
+                        fetchHealthData()
                         simpleSuccessHaptic()
-                        self.isActive = true
                     }, label: {
                         Text("Get Started")
                             .font(Font.custom("Avenir", size: 24))
@@ -79,10 +211,11 @@ struct SelectionView: View {
 
 
                 }
+                .padding()
             }
             .background(ZStack{
                 Image("ScreenBackground").aspectRatio(contentMode: .fit).border(.black)
-            }.background(.black))
+            }.background(Color("BrandBlack")))
         }
     }
 }
@@ -92,3 +225,4 @@ struct SelectionView_Previews: PreviewProvider {
         SelectionView()
     }
 }
+
